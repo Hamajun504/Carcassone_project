9b8a0043a2b92134b1ceb86cc3e95c6c83b0cce4
Yaroslav Orlov <orlov.iaa@phystech.edu>
2022-11-22 19:54:50 +0300

    something3

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ import pygame
 import config
 import player
 import field
+import card
 
 
 class Game:
@@ -11,18 +12,21 @@ class Game:
         self.clock = pygame.time.Clock()
         self.finished = False
         self.field = field.Field()
+        self.next_card = card.Card()
 
     def draw(self):
-        # TODO
-        pass
+        self.screen.fill(config.WHITE)
+        self.field.draw(self.screen)
 
     def event_processing(self):
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 finished = True
             elif event.type == pygame.MOUSEBUTTONDOWN:
-                if config.FIELD_COORDS[0] <= event.pos[0] < config.FIELD_COORDS[0] + config.FIELD_WIDTH * config.GRID and
-
+                if config.FIELD_COORDS[0] <= event.pos[0] < config.FIELD_COORDS[0] + config.FIELD_WIDTH * config.GRID and \
+                        config.FIELD_COORDS[1] <= event.pos[1] < config.FIELD_COORDS[1] + config.FIELD_HEIGHT * config.GRID:
+                    field.place((event.pos[0] - config.FIELD_COORDS[0], event.pos[1] - config.FIELD_COORDS[1]))
+                    self.next_card = card.Card()
 
     def run(self):
         while not self.finished:
@@ -31,4 +35,5 @@ class Game:
             self.event_processing()
 
             # TODO
+
 
--- main.py
+++ main.py
@@ -5,5 +5,6 @@ import game
 
 pygame.init()
 screen = pygame.display.set_mode((config.SCREEN_WIDTH, config.SCREEN_HEIGHT))
-
+g = game.Game(screen)
+g.run()
 pygame.quit()
